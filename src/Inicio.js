2cf397679d51573fde0246eb2bd7a770fd88a376
Tomiguts <tmfattal@gmail.com>
2025-05-06 22:44:16 -0500

    cambio de lineas

--- src/Inicio.js
+++ src/Inicio.js
@@ -10,8 +10,8 @@ function App() {
       <div className="navbar">
         <a className="Nombre">MercaU</a>
         <a className="Inicio" href="#Inicio" onClick={() => console.log("Enlace clickeado")}>Inicio</a>
-        <a className="Vender" href="#Vender">Vender</a>
-        <a className="Comprar"href="#Comprar">Comprar</a>
+        <a className="Inicio" href="#Vender">Vender</a>
+        <a className="Inicio"href="#Comprar">Comprar</a>
 
         <button className="login-btn" onClick={() => window.location.href='https://youtube.com/a/chat/s/62cfa149-33d0-4bcc-879c-d689d0ff6910'}>
         Iniciar Sesión
