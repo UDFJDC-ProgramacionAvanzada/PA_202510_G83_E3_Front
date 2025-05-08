c0b403e82c61f9b58c0bf4137851e409e20ac7d8
Tomiguts <tmfattal@gmail.com>
2025-05-07 19:38:15 -0500

    actualizacion para hacer responsive la barra navbar que va en el inicio

--- src/Inicio.js
+++ src/Inicio.js
@@ -1,30 +1,48 @@
 import React from 'react';
+import {useState} from 'react';
 import './Inicio.css';
 import HU13 from './components/HU13.js';
 import Mapa from './components/Mapa.js' 
 
 function App() {
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
     <body>
       <div className="navbar">
-        <a className="Nombre">MercaU</a>
-        <a className="Inicio" href="#Inicio" onClick={() => console.log("Enlace clickeado")}>Inicio</a>
-        <a className="Inicio" href="#Vender">Vender</a>
-        <a className="Inicio"href="#Comprar">Comprar</a>
-
-        <button className="login-btn" onClick={() => window.location.href='https://youtube.com/a/chat/s/62cfa149-33d0-4bcc-879c-d689d0ff6910'}>
-        Iniciar Sesión
-        </button>
-        <a className="profile-link" href="#MiPerfil">Mi Perfil</a>
+        <a className="Nombre" href="http://localhost:3000">MercaU</a>
+        
+        {/* Menú Hamburguesa (solo visible en móvil) */}
+        <div className="hamburger" onClick={toggleMenu}>
+          ☰
+        </div>
+        
+        {/* Contenedor de los elementos del menú */}
+        <div className={`nav-links ${menuOpen ? 'active' : ''}`}>
+          
+          <a className="nav-item" href="#Inicio" onClick={() => console.log("Enlace clickeado")}>Inicio</a>
+          <a className="nav-item" href="#Vender">Vender</a>
+          <a className="nav-item" href="#Comprar">Comprar</a>
+          
+          <a className="nav-item profile-link" href="#MiPerfil">Mi Perfil</a>
+          
+          <button className="nav-item login-btn" onClick={() => window.location.href='https://youtube.com/a/chat/s/62cfa149-33d0-4bcc-879c-d689d0ff6910'}>
+            Iniciar Sesión
+          </button>
+        </div>
       </div>
+      
       <div className="hu13-container">
-      <HU13 />
+        <HU13 />
       </div>    
       <div className="mapa-container">
         <Mapa/>
       </div>
-
     </body>
   );
 }
