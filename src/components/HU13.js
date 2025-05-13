f3c11b5130eb70318892e508ae33fa97f1e43bb0
santy <santivargas222@gmail.com>
2025-05-12 20:39:29 -0500

    se modifico el archivo css de HU13 para que fuera responsiva y no generara conflicto al hacerse zoom

--- src/components/HU13.js
+++ src/components/HU13.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './HU13.css';
 
-
 function HU13() {
     return (
         <div className="hu13-container">
-            <div className="left-content">
-                <h2 className="titulo_de_aviso">Compra, vende e impulsa a tu universidad</h2>
-                <input type="text" placeholder="¿Qué estás buscando hoy?" className="search-input" />
+            <div className="content-wrapper">
+                <div className="left-content">
+                    <h2 className="titulo_de_aviso">Compra, vende e impulsa a tu universidad</h2>
+                    <input type="text" placeholder="¿Qué estás buscando hoy?" className="search-input" />
+                </div>
+                <div className="right-content">
+                    <img src="/imagenamigos.png" alt="Imagen de amigos" className="imagen_amigos" />
+                </div>
             </div>
-            <img src="/imagenamigos.png" alt="Imagen de amigos" className="imagen_amigos" />
         </div>
     );
 }
