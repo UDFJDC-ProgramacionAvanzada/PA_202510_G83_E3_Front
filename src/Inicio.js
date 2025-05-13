865a31b978eebbca45240dceb13ff68b62aa3b32
Tomiguts <tmfattal@gmail.com>
2025-05-12 22:07:43 -0500

    separacion del navbar como componente para implementarlo en futuras pestañas

--- src/Inicio.js
+++ src/Inicio.js
@@ -1,42 +1,15 @@
 import React from 'react';
-import {useState} from 'react';
 import './Inicio.css';
 import HU13 from './components/HU13.js';
 import Mapa from './components/Mapa.js' 
+import Navbar from './components/Navbardef.js';
 
 function App() {
-
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  }
-
   return (
     <body>
       <div className="navbar">
-        <a className="Nombre" href="http://localhost:3000">MercaU</a>
-        
-        {/* Menú Hamburguesa (solo visible en móvil) */}
-        <div className="hamburger" onClick={toggleMenu}>
-          ☰
-        </div>
-        
-        {/* Contenedor de los elementos del menú */}
-        <div className={`nav-links ${menuOpen ? 'active' : ''}`}>
-          
-          <a className="nav-item" href="#Inicio" onClick={() => console.log("Enlace clickeado")}>Inicio</a>
-          <a className="nav-item" href="#Vender">Vender</a>
-          <a className="nav-item" href="#Comprar">Comprar</a>
-          
-          <a className="nav-item profile-link" href="#MiPerfil">Mi Perfil</a>
-          
-          <button className="nav-item login-btn" onClick={() => window.location.href='https://youtube.com/a/chat/s/62cfa149-33d0-4bcc-879c-d689d0ff6910'}>
-            Iniciar Sesión
-          </button>
-        </div>
+        <Navbar/>
       </div>
-      
       <div className="hu13-container">
         <HU13 />
       </div>    
@@ -46,6 +19,5 @@ function App() {
     </body>
   );
 }
-
 
 export default App;
---
+++ src/components/Navbardef.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {useState} from 'react';
+import './Navbar.css';
+
+
+function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+<div className="navbar">
+    
+    <a className="Nombre" href="http://localhost:3000">MercaU</a>
+    {/* Menú Hamburguesa (solo visible en móvil) */}
+    <div className="hamburger" onClick={toggleMenu}>
+        ☰
+    </div>
+    
+{/* Contenedor de los elementos del menú */}
+    <div className={`nav-links ${menuOpen ? 'active' : ''}`}>
+        <a className="nav-item profile-link" href="#MiPerfil">Mi Perfil</a>
+        <a className="nav-item" href="#Inicio" onClick={() => console.log("Enlace clickeado")}>Inicio</a>
+        <a className="nav-item" href="#Vender">Vender</a>
+        <a className="nav-item" href="#Comprar">Comprar</a>
+        <button className="nav-item login-btn" onClick={() => window.location.href='https://youtube.com/a/chat/s/62cfa149-33d0-4bcc-879c-d689d0ff6910'}>
+        Iniciar Sesión
+        </button>
+    </div>
+</div>
+    );
+}
+export default Navbar;
