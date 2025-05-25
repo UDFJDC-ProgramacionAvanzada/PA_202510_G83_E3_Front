05512125764eaf6dd44c6f2f57391b5e67ee1405
santy <santivargas222@gmail.com>
2025-05-25 15:36:39 -0500

    se agrega la barra superior de la seccion de vender

--- src/pages/Vender.js
+++ src/pages/Vender.js
@@ -3,7 +3,13 @@ import './Vender.css';
 
 function Vender() {
     return (
-        <h1>HOLA</h1>
+        <div className="vender-container">
+            <h1 className='texto-vender'>VENDER</h1>
+            <div className='register'>
+                <span className='register-text'>Eres nuevo?</span>
+                <button className='register-button'>Registrate</button>
+            </div>
+        </div>
     )
 }
 
